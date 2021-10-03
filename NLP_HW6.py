2f882e9498e3803c1f286d48e99c4209ec52d01a
Sina Dabiri <sina.dabiri@berkeley.edu>
2021-10-03 12:52:23 -0400

    answers for 1d jaccard similarity

--- NLP_HW6.py
+++ NLP_HW6.py
@@ -22,9 +22,9 @@ to vectorize the two texts using the CountVectorizer. Use n-gram size of 1-3 for
 
 from sklearn.feature_extraction.text import CountVectorizer
 corpus = [text1, text2]
+# CountVectorizer coverts the collection of text, corpus, into a matrix of token counts
 vectorizer = CountVectorizer(ngram_range=(1, 3))
 vectorized_corpus = vectorizer.fit_transform(corpus)
-# print(vectorized_corpus)
 
 '''
 c) Compute the cosine similarity between the texts (sklearn provides a function for it).
@@ -34,4 +34,32 @@ vect1 = vectorized_corpus[0]
 vect2 = vectorized_corpus[1]
 cos_similarity = cosine_similarity(vect1, vect2)
 
-print(cos_similarity)
+print("The cosine similarity is: ", cos_similarity)
+
+'''
+d) Compute the jaccard similarity between text1 and text2
+'''
+from sklearn.metrics import jaccard_score
+import numpy as np
+print(np.shape(vect1), '\n', np.shape(vect2))
+print(np.dtype(vect1), '\n', np.dtype(vect2))
+
+# remove white space, tokenize
+text1_token = text1.strip().split()
+text2_token = text2.strip().split()
+text1_set = set(text1_token)
+text2_set = set(text2_token)
+print("Tokenized text1 as a set is: ", text1_set)
+print("Tokenized text2 as a set is: ", text2_set)
+
+# Find the intersection of the two vectors
+intersec = text1_set.intersection(text2_set)
+
+# Find the union of the two vectors
+union = text1_set.union(text2_set)
+
+# Calculate Jaccard similarity
+jaccard_similarity = float(len(intersec)/len(union))
+print("The Jaccard similarity is: ", jaccard_similarity)
+
+
